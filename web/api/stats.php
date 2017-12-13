52460a01807b3a89affc5b24600b30d357610327
Almic <mick.ashton@flare-esports.net>
2017-12-13 12:54:33 -0700

    Monthly Offset
    Added the option to only pull a certain number of months from yearly entries. Returns up to 6 months at a time.

--- web/api/stats.php
+++ web/api/stats.php
@@ -9,6 +9,20 @@ if (isset($_GET["steamid"])) {
 } else {
     die("Missing required field `steamid`");
 }
+// We can send up to 6 months of changes at a time, and with the `offset` we can begin later
+if (isset($_GET["offset"])) {
+    if (!is_int($_GET["offset"])) {
+        die("Offset is not a valid number.");
+    }
+    $offset = intval($_GET["offset"]);
+    if ($offset < 1) {
+        die("Minimum offset value is 1");
+    }
+    if ($offset > 54) {
+        die("Maximum offset value is 54");
+    }
+    $offset--;
+}
 
 require_once("dbInf.php");
 
@@ -35,6 +49,14 @@ if ($result->num_rows === 0) {
 }
 
 $result = $result->fetch_object();
+if (isset($offset)) {
+    // Only asking for the monthly stats
+    $arr = json_decode($result->history)->entries_year;
+    if (count($arr) < $offset) {
+        pretty_exit(array());
+    }
+    pretty_exit(array_slice($arr, $offset, 6));
+}
 
 $object = json_decode('{}');
 $object->{"id"} = strval($result->id); // Force string just to be safe
@@ -42,7 +64,9 @@ $object->{"type"} = $result->type;
 if ($result->type === "user") {
     $object->{"global"} = json_decode($result->global);
 }
-$object->{"history"} = json_decode($result->history);
+$history = json_decode($result->history);
+$object->{"daily"} = $history->entries_month;
+$object->{"monthly"} = array_slice($history->entries_year, 0, 6);
 $object->{"current"} = json_decode($result->current);
 
 pretty_exit($object);
