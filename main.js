189488f4ec3405298c3a09543988389b1bdbb122
Almic <mick.ashton@flare-esports.net>
2018-02-12 09:11:40 -0700

    fix(web): Fix port bind

--- main.js
+++ main.js
@@ -16,4 +16,4 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(3000, () => console.log('Listening on port 3000!'))
+app.listen(process.env.PORT || 8080, () => console.log('Listening on port 3000!'))
