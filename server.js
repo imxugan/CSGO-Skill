834129b392f3d3ae68600325ae66687347fad3cd
Almic <mick.ashton@flare-esports.net>
2018-02-24 09:15:18 -0700

    feat(server): Add OpenID authentication and an assistant module
    This also includes some tests for the MongoDB connection. We should also be able to run a full login
    test and get our steamid as output to the browser.
    
    progress on #18

--- server.js
+++ server.js
@@ -11,22 +11,24 @@
 
 /* BEGIN MODULES */
 const express = require('express')
-const mongojs = require('mongojs')
-const crypto = require('crypto')
+    , mongojs = require('mongojs')
+    , LightSteamID = require('./lib/openid.js')
+    , assistant = require('./lib/assistant.js')
 
 /** END MODULES **/
 
 /* BEGIN SETUP */
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-/*
-  @Contributors, be sure you change this to your local
-  connection url, and back to process.env.MONGOURL before
-  submitting a pull request!
-  For help with setting up a local MongoDB, see the wiki on
-  the repository linked at the top of this file.
- */
-const MONGOURL = process.env.MONGOURL
+const MONGOURL = assistant.MONGOURL
+const STEAMKEY = assistant.STEAMKEY
+
+const skilldb = mongojs(MONGOURL)
+skilldb.Example.find((err, docs) => {
+    console.log(docs)
+});
 
 /** END SETUP **/
 
@@ -34,12 +36,40 @@ const MONGOURL = process.env.MONGOURL
 /* BEING ROUTING */
 app.get('/', (req, res) => {
     res.send('Hello World!')
+    skilldb.Example.find((err, docs) => {
+        res.send(`\nDoc located, _id: ${docs[0]._id}, test: ${docs[0].test}`)
+    })
+})
+
+app.get('/login', (req, res) => {
+
+    if (req.subdomains[0] === 'api') {
+        // Device request
+        const openid = new LightSteamID('http://api.csgo-skill.com/login', req)
+        if (!openid.mode) {
+            res.location(openid.authUrl)
+        } else if(openid.mode == 'cancel') {
+            res.send('canceled')
+        } else {
+            if (openid.validate()) {
+                var steamid = openid.steam_id
+                res.send(steamid)
+            } else if (openid.errno !== 0) {
+                res.send(`Error occured during validation: [${openid.errno}] ${openid.error}`)
+            }
+        }
+    } else {
+        // Website request
+        const openid = new LightSteamID('http://www.csgo-skill.com/login', req)
+    }
+
+    //res.send(openid.authUrl())
 })
 /** END ROUTING **/
 
 const server = app.listen(process.env.PORT || 8080, () => {
     console.log('Listening on port ' + server.address().port)
-    var db = mongojs(MONGOURL)
+
 })
 
 // Don't forget to change the updated date!
