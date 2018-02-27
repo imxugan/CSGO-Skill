7bd5c2d3f296ae642a780825f9be18a89b837101
Almic <mick.ashton@flare-esports.net>
2018-02-26 20:40:34 -0700

    feat(server): Update MongoDB methods and API route handling
    We've ditched mongojs in pursuit of using MongoDB v3.0 over v2.2. I also wrote the helper file
    `mongo.js` to store a few levels of the callback hell that MongoDB insists on using. While I would
    love to write at least three callbacks everytime I needed to ask the server a question, I'm sure
    others would rather skip as many as they can. The router `api.js` will now handle all requests for
    the 'api' subdomain, and `server.js` will probably continue handling all normal requests. Some minor
    changes to `assistant.js`, too.

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-  /********************************************************
-  *    This file is licensed under the MIT 2.0 license    *
-  *           Last updated February 23rd, 2018            *
-  *   *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *   *
-  *    Please check out the full repository located at    *
-  *   http://github.com/almic/CSGO-Skill for some other   *
-  * important things like the User Agreement and Privacy  *
-  *  Policy, as well as some helpful information on how   *
-  *     you can contribute directly to the project :)     *
-  ********************************************************/
+    /*********************************************************
+     *    This file is licensed under the MIT 2.0 license    *
+     *           Last updated February 26th, 2018            *
+     *   *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *   *
+     *    Please check out the full repository located at    *
+     *   http://github.com/almic/CSGO-Skill for some other   *
+     * important things like the User Agreement and Privacy  *
+     *  Policy, as well as some helpful information on how   *
+     *     you can contribute directly to the project :)     *
+     *********************************************************/
 
 /* BEGIN MODULES */
 const assistant = require('./lib/assistant.js')
 var startup = assistant.timer('Server Started')
 const express = require('express')
-    , mongojs = require('mongojs')
+    , skill = require('./lib/mongo.js')
+    , api = require('./lib/api.js')
     , LightSteamID = require('./lib/openid.js')
 
 /** END MODULES **/
@@ -25,14 +26,19 @@ app.use(express.urlencoded({ extended: true }))
 
 const MONGOURL = assistant.MONGOURL
 const STEAMKEY = assistant.STEAMKEY
-var skilldb = null
-var hasDb = false
 
 try {
-    skilldb = mongojs(MONGOURL)
-    hasDb = true
+    skill.connect(MONGOURL)
+    app.use(api({db: skill, apikey: STEAMKEY}))
+    skill.db('collection:Testing', (err, col) => { if (err) throw err;
+        col.find().toArray((err, docs) => { if (err) throw err;
+            if (docs[0].test === 'abc123') {
+                console.log(`Successfully connected to MongoDB`)
+            }
+        })
+    })
 } catch (e) {
-    assistant.errorOut(10, e.message)
+    assistant.e(100, e)
 }
 
 /** END SETUP **/
@@ -41,52 +47,28 @@ try {
 /* BEING ROUTING */
 app.get('/', (req, res) => {
     var timer = assistant.timer()
+
     var m = 'Hello World!';
-    skilldb.Example.find((err, docs) => {
-        m += `\nDoc located, _id: ${docs[0]._id}, test: ${docs[0].test}`
-        res.send(m)
-        timer.stop()
-    })
-    //res.send(m)
+    res.send(m)
+
+    timer.stop()
 })
 
 app.get('/login', (req, res) => {
-    var m = '';
     var timer = assistant.timer()
-    if (req.subdomains[0] === 'api') {
-        // Device request
-        const openid = new LightSteamID('http://api.csgo-skill.com/login', req)
-        if (!openid.mode) {
-            res.status(302).location(openid.authUrl)
-            res.send()
-            timer.stop()
-        } else if(openid.mode == 'cancel') {
-            m += 'canceled'
-        } else {
-            if (openid.validate()) {
-                var steamid = openid.steam_id
-                m += steamid
-            } else if (openid.errno !== 0) {
-                assistant.errorOut(openid.errno, openid.error)
-                m += 'Error occured during validation'
-            }
-        }
-    } else {
-        // Website request
-        const openid = new LightSteamID('http://www.csgo-skill.com/login', req)
-    }
 
-    if (!res.headersSent) {
-        res.send(m)
-        timer.stop()
-    }
+    var m = '';
+    const openid = new LightSteamID('http://www.csgo-skill.com/login', req)
+
+    res.send(m)
+
+    timer.stop()
 })
 /** END ROUTING **/
 
 const server = app.listen(process.env.PORT || 8080, () => {
     startup.stop()
     console.log('Listening on port ' + server.address().port)
-
 })
 
 // Don't forget to change the updated date!
